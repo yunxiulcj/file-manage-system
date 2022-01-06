31400db4fbcffb2df45222f7c50aaed43d7ea92b
liucanjian <canjian.liu@mountec.net>
2022-01-06 11:26:11 +0800

    s

---
+++ src/views/process/approval.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box">
+    <page-frame title="审批详情" icon="approval"></page-frame>
+  </div>
+</template>
+<script>
+import pageFrame from '../../components/pageFrame.vue'
+export default {
+  name: 'approval',
+  components: { pageFrame },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
