d81988f8afb543f90b1b811a23be2bad5ee85881
liucanjian <canjian.liu@mountec.net>
2022-01-13 18:52:03 +0800

    s

--- src/app/api/modules/systemManage.js
+++ src/app/api/modules/systemManage.js
@@ -1,1 +1,38 @@
-export default {}
+export default {
+  fileSetting: {
+    url: '/system/fileSetting',
+  },
+  diskPathSetting: {
+    url: '/system/diskPathSetting',
+  },
+  getSystemLogList: {
+    url: '/system/getSystemLogList',
+  },
+  deleteSystemLog: {
+    url: '/system/deleteSystemLog',
+  },
+  getFileOperationLogList: {
+    url: '/system/getFileOperationLogList',
+  },
+  saveAccount: {
+    url: '/system/saveAccount',
+  },
+  domainAccountMatch: {
+    url: '/system/domainAccountMatch',
+  },
+  getAccountDetail: {
+    url: '/system/getAccountDetail',
+  },
+  getAccountList: {
+    url: '/system/getAccountList',
+  },
+  deleteAccount: {
+    url: '/system/deleteAccount',
+  },
+  emailSetting: {
+    url: '/system/emailSetting',
+  },
+  getSetting: {
+    url: '/system/getSetting',
+  },
+}
